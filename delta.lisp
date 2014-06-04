b698fd99d3886006bd633efad91836dbf36afc02
Stuart Olsen <sjo@lantea>
2014-06-04 02:44:19 -0500

    Tossed in what I've done so far

---
+++ delta.lisp
@@ -0,0 +1,123 @@
+(require '#:sj-lisp)
+
+(eval-when (:compile-toplevel)
+  (import '(sj-lisp:exchangef
+            sj-lisp:setf->changed
+            sj-lisp:with-hygienic-names)))
+
+(defclass language ()
+  ())
+
+(defclass empty-language (language)
+  ())
+
+(defclass null-language  (language)
+  ())
+
+(defclass terminal-language (language)
+  ())
+
+(defclass repeat-language (language)
+  ())
+
+
+
+(defclass delta-recursive ()
+  ((delta-cache :type boolean
+                :initform nil
+                :accessor delta-cache)
+   (delta-fixed :type boolean
+                :initform nil
+                :accessor delta-fixed)
+   (delta-visited :type boolean
+                  :initform nil
+                  :accessor delta-visited)))
+
+(defun delta-recursivep (L)
+  (typep L 'delta-recursive))
+
+(defclass alternate-language (language delta-recursive)
+  ((left :type language
+         :initarg :left
+         :accessor left)
+   (right :type language
+          :initarg :right
+          :accessor right)))
+
+(defclass catenate-language (language delta-recursive)
+  ((left :type language
+         :initarg :left
+         :accessor left)
+   (right :type language
+          :initarg :right
+          :accessor right)))
+
+(defun reset-visitedness (L)
+  (when (delta-recursivep L)
+    (when (sj-lisp:exchangef (delta-visited L) nil)
+      (reset-visitedness (left L))
+      (reset-visitedness (right L)))))
+
+
+
+(defmacro or/or (first &rest rest)
+  (if rest
+      (with-hygienic-names (fvalue1 fvalue2 rvalue1 rvalue2)
+        `(multiple-value-bind (fvalue1 fvalue2)
+             ,first
+           (if fvalue1
+               (values fvalue1 fvalue2)
+             (multiple-value-bind (rvalue1 rvalue2)
+                 (or/or ,@rest)
+               (values (or fvalue1 rvalue1)
+                       (or fvalue2 rvalue2))))))
+    first))
+
+(defmacro and/or (first &rest rest)
+  (if rest
+      (with-hygienic-names (fvalue1 fvalue2 rvalue1 rvalue2)
+        `(multiple-value-bind (fvalue1 fvalue2)
+             ,first
+           (if (not fvalue1)
+               (values fvalue1 fvalue2)
+             (multiple-value-bind (rvalue1 rvalue2)
+                 (and/or ,@rest)
+               (values (and fvalue1 rvalue1)
+                       (or fvalue2 rvalue2))))))
+    first))
+
+(defun nullablep (L)
+  (labels ((delta (L)
+             (declare (ftype (function (language) (values boolean boolean)) delta/changed))
+             (etypecase L
+               (empty-language     nil)
+               (null-language      t)
+               (terminal-language  nil)
+               (repeat-language    t)
+               (alternate-language (or/or (delta/changed (left L))
+                                          (delta/changed (right L))))
+               (catenate-language  (and/or (delta/changed (left L))
+                                           (delta/changed (right L))))))
+           (delta/changed (L)
+             (cond ((not (delta-recursivep L))
+                    (delta L))
+                   ((delta-fixed L)
+                    (delta-cache L))
+                   ((sj-lisp:exchangef (delta-visited L) t)
+                    (delta-cache L))
+                   (t
+                    (sj-lisp:multiple-value-let* ((new-delta changed1 (delta L))
+                                                  (changed2
+                                                   (sj-lisp:setf->changed (delta-cache L) new-delta)))
+                      (when (eql new-delta t) ;; Optimization - at the top of the lattice
+                        (setf (delta-fixed L) t))
+                      (values new-delta (or changed1 changed2)))))))
+    (multiple-value-bind (result changed)
+        (delta/changed L)
+      (reset-visitedness L)
+      (if changed
+          (nullablep L)
+        (progn
+          (when (delta-recursivep L)
+            (setf (delta-fixed L) t))
+          result)))))
---
+++ test-case.lisp
@@ -0,0 +1,79 @@
+(defmethod print-object :around ((o language) str)
+  (declare (ignore o str))
+  (let ((*print-circle* t)
+        (*print-level* 20))
+    (call-next-method)))
+
+(defmethod print-object ((o empty-language) str)
+  (format str "{}"))
+(defmethod print-object ((o null-language) str)
+  (format str "{e}"))
+(defmethod print-object ((o terminal-language) str)
+  (format str "{c}"))
+(defmethod print-object ((o repeat-language) str)
+  (format str "{c}*"))
+
+(defmethod print-object ((o alternate-language) str)
+  (format str "~A" (list 'or (left o) (right o))))
+(defmethod print-object ((o catenate-language) str)
+  (format str "~A" (list 'and (left o) (right o))))
+
+
+
+(defun make-testcase (n)
+  (labels ((random-language ()
+             (let ((p (random 100)))
+               (make-instance
+                 (cond ((< p 45) 'alternate-language)
+                       ((< p 90) 'catenate-language)
+                       ((< p 95)  'empty-language)
+                       (t        'null-language))))))
+    (let ((v (make-array n :element-type '(or language null))))
+      (loop
+         for i from 0 below n
+         do (setf (aref v i) (random-language)))
+      (loop
+         for L across v
+         when (delta-recursivep L)
+           do (setf (left L) (aref v (random n))
+                    (right L) (aref v (random n))))
+      (find-if #'delta-recursivep v))))
+
+(defun map-language (f L)
+  (labels ((do-it (L visited)
+             (unless (exchangef (gethash L visited) t)
+               (funcall f L)
+               (when (delta-recursivep L)
+                 (do-it (left L) visited)
+                 (do-it (right L) visited)))))
+    (do-it L (make-hash-table))))
+
+(defun lreset (L)
+  (when (delta-recursivep L)
+    (setf (delta-cache L) nil
+          (delta-fixed L) nil
+          (delta-visited L) nil)))
+
+(defun lreset-all (L)
+  (map-language #'lreset L))
+
+(defgeneric verify (L)
+  (:method ((L empty-language))
+    (assert (not (nullablep L))))
+  (:method ((L null-language))
+    (assert (nullablep L)))
+  (:method ((L empty-language))
+    (assert (not (nullablep L))))
+  (:method ((L repeat-language))
+    (assert (nullablep L)))
+  (:method ((L alternate-language))
+    (assert (eql (nullablep L)
+                 (or (nullablep (left L))
+                     (nullablep (right L))))))
+  (:method ((L catenate-language))
+    (assert (eql (nullablep L)
+                 (and (nullablep (left L))
+                      (nullablep (right L)))))))
+
+(defun verify-all (L)
+  (map-language #'verify L))
